1ab3cced7ffa4fd7be835107db707ca3c3deb4a6
Ayanna Curwen <yanwen2627@gmail.com>
2024-08-07 21:43:02 -0400

    Create calculator.sh
    Initial sudo code

---
+++ calculator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Basic Calculator
+
+# read two integer inputs from the user
+#read first integer from user
+#read second integer from user
+#read operation option from user #specify the options for the user like 'x' '/' or write out 'multiplication'
+
+
+#if addition, add integer 1 and integer 2 and display the answer
+
+#if (else) subtraction, subtract integer 1 from integer 2 and display the answer
+
+#if (else) multiplication, multiply integer 1 and integer 2 and display the answer
+
+#if (else) division, divide integer 1 by integer 2 and display the answer
+
+
+
+
