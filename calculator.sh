50723794c092335c159203fdb876d6b9e67ad83d
Ayanna Curwen <yanwen2627@gmail.com>
2024-08-08 02:40:20 -0400

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #Basic Calculator
 
-# read two integer inputs from the user
+#read inputs from user
+
 #read first integer from user
 #read second integer from user
-#read operation option from user #specify the options for the user like 'x' '/' or write out 'multiplication'
+#read operation option from user and specify the options for the user like '*' '/' or 'multiplication'
 
+#arithmetic
 
 #if addition, add integer 1 and integer 2 and display the answer
-
 #if (else) subtraction, subtract integer 1 from integer 2 and display the answer
-
 #if (else) multiplication, multiply integer 1 and integer 2 and display the answer
-
 #if (else) division, divide integer 1 by integer 2 and display the answer
+#if dividing by 0, include an error message
+
+
+
 
 
 
